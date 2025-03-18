af02cb3dd244b5183b87c701aa1dac176f7254fb
phanqui21 <phanqui2021@gmail.com>
2025-03-18 07:18:49 +0700

    init_firebase

--- alarmApp/lib/main.dart
+++ alarmApp/lib/main.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
 import 'package:alarm/alarm.dart';
+import 'package:alarm/firebase_options.dart';
 import 'package:alarm_with_speech/screens/home.dart';
 import 'package:alarm_with_speech/utils/logging.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   setupLogging(showDebugLogs: true);
